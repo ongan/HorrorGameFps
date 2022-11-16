d324b41434e72014443817c41574d0483b1d46ab
ongan <mehmetongan86@gmail.com>
2022-11-16 20:54:35 +0300

    init

---
+++ Player/DoorOpen.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    public float theDistance;
+    public GameObject actionKey;
+    public GameObject actionText;
+    public GameObject hinge;
+    public AudioSource doorSound;
+
+    
+    void Update()
+    {
+        theDistance = PlayerRay.distanceFromTarget;
+        
+    }
+    private void OnMouseOver()
+    {
+        if (theDistance <= 2)
+        {
+            actionKey.SetActive(true);
+            actionText.SetActive(true);
+        }
+        else
+        {
+            actionKey.SetActive(false);
+            actionText.SetActive(false);
+        }
+        if (Input.GetButton("Action"))
+        {
+            if (theDistance <= 2)
+            {
+                this.gameObject.GetComponent<BoxCollider>().enabled = false;
+                actionKey.SetActive(false);
+                actionText.SetActive(false);
+                hinge.GetComponent<Animation>().Play("MyDoorAnim");
+                doorSound.Play();
+            }
+        }
+    }
+    private void OnMouseExit()
+    {
+        actionKey.SetActive(false);
+        actionText.SetActive(false);
+    }
+   
+
+}
---
+++ Player/PlayerRay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRay : MonoBehaviour
+{
+    public static float distanceFromTarget;
+    //bir çok interaktif nesnenin uzaklığını ölçmek için değişkeni static yaptık
+    //ve diğer scriptlerden ulaşabilmek için yaptık
+    //yani hedeften uzaklık olarak hesaplamak için
+    public float toTarget;
+    // toTarget gördüğümüz her nesneye olan uzaklık olacak duvar yer zemin tavan vs gibi hesaplaması için
+    
+    void Update()
+    {
+        RaycastHit hit; //bir ışın demeti oluşturduk ve buna hit adını verdik
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+        //Physics.Raycast bir nesneye çarpıp çarpmadığımızı kontrol içinm yazdık
+        //burada fizik objesinden bir ışın yolluyoruz
+        //ve transform.position (burada bizim poziyonumuzdan) yani orijin olan merkezden
+        //transform.TransformDirection(Vector3.forward), yani önümüze doğru bir ışın çıkar
+        //out hit ise burada bize verdiği çıktı
+        {
+            toTarget = hit.distance;
+            //toTarget dediğimizi ışının uzunluğuna atıyoruz
+            //yani burada toTarget mesela duvarsa hit.distance da duavara olan uzaklıktır ve bunu hesaplar
+            distanceFromTarget = toTarget;
+            //burada da ışının toTarget a olan uzaklığı 
+        }
+    }
+}
